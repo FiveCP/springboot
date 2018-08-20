765c11ccc6de6b935cf2cf9fb02c01aa04ad3675
FiveCP <2318263528@qq.com>
2018-08-20 16:38:26 +0800

    Jenkins

---
+++ src/test/java/com/wcp/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,34 @@
+package com.wcp.springboot;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationTests {
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
+	@Test
+	public void save() {
+		ArrayList<String> hs = new ArrayList<>();
+		hs.add("2");hs.add("3");hs.add("7");
+		String[] hs1 = (String[])hs.toArray(new String[hs.size()]);
+		stringRedisTemplate.opsForValue().set("wcp", "FiveCP");
+		stringRedisTemplate.opsForSet().add("CP",hs1);
+		Assert.assertEquals(stringRedisTemplate.opsForValue().get("wcp"),"FiveCP");
+		Assert.assertTrue(stringRedisTemplate.opsForSet().isMember("CP","2"));
+	}
+	
+ 
+
+
+}
